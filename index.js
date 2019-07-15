ac7b0edcf0c7aa85ab05ecdaeb6389ce98af65a0
Peter Robinson <Peter.Robinson@softwire.com>
2019-07-15 15:00:33 +0100

    FizzBuzz part 2 answers

--- index.js
+++ index.js
@@ -3,8 +3,23 @@ function fb(x){
     if (x%3 == 0){
         ans.push("Fizz")
     }
+    if (x%13 == 0){
+        ans.push("Fezz")
+    }
     if (x%5 == 0){
         ans.push("Buzz")
+    }
+    if (x%7 == 0){
+        ans.push("Bang")
+    }
+    if (x%11 == 0){
+        ans = ["Bong"];
+        if (x%13 == 0){
+            ans = ["FezzBong"];
+        }
+    }
+    if (x%17 == 0){
+        ans.reverse()
     }
     if (ans.length == 0){
         console.log(x)
@@ -12,6 +27,6 @@ function fb(x){
         console.log(ans.join(""))
     }
 }
-for (var i = 1; i < 100; i++){
+for (var i = 1; i < 256; i++){
     fb(i);
 }
