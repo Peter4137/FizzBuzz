52525bd103f2e9877f16c8c541b1ae0814608724
Peter Robinson <Peter.Robinson@softwire.com>
2019-07-15 14:52:39 +0100

    Basic FizzBuzz program

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-
+function fb(x){
+    let ans = [];
+    if (x%3 == 0){
+        ans.push("Fizz")
+    }
+    if (x%5 == 0){
+        ans.push("Buzz")
+    }
+    if (ans.length == 0){
+        console.log(x)
+    }else{
+        console.log(ans.join(""))
+    }
+}
+for (var i = 1; i < 100; i++){
+    fb(i);
+}
